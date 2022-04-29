d51587d2726f4e406f7a1e9d7c7823d896f2a12a
ali kabbadj <alikabbadj1994@gmail.com>
2022-04-29 17:13:45 +0000

    Implemeted initial Nine Anime Service

---
+++ PandoranimeService/Services/NineAnime/NineAnimeService.cs
@@ -0,0 +1,19 @@
+﻿using PandoranimeService.Interfaces.NineAnime;
+using PandoranimeService.Models._9Anime;
+using PandoranimeService.Services.Scappers;
+
+namespace PandoranimeService.Services.NineAnime
+{
+    public class NineAnimeService : INineAnimeService
+    {
+        public async Task<List<AnimeByPageModel>> GetAnimesByPage(int pageIndex)
+        {
+            return await PuppeteerSharpService.NineAnimeGetListByPageId(pageIndex);
+        }
+
+        public async Task<int> GetMaxPage()
+        {
+            return await PuppeteerSharpService.NineAnimeGetMaxPage();
+        }
+    }
+}
