9fa3a9a3d4858c115af6c614573e262ac78ec117
ali kabbadj <alikabbadj1994@gmail.com>
2022-04-29 16:57:50 +0000

    implemented PuppeteerSharpService _Open/Close chromeium -9Anime Scapping methdes : -MaxPage,AnimeList by page

---
+++ PandoranimeService/Models/9Anime/AnimeByPageModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PandoranimeService.Models._9Anime
+{
+    public class AnimeByPageModel
+    {
+        private string _name;
+        private string _url;
+        private string _imageUrl;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Url
+        {
+            get { return _url; }
+            set { _url = value; }
+        }
+
+        public string ImageUrl
+        {
+            get { return _imageUrl; }
+            set { _imageUrl = value;}
+        }
+    }
+}
