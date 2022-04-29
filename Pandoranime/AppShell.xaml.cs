a6fe38f99708a14e139dd312912617ab66b064ea
ali kabbadj <alikabbadj1994@gmail.com>
2022-04-29 15:48:01 +0000

    Add project files.

---
+++ Pandoranime/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Pandoranime
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PandoranimeService/Platforms/MacCatalyst/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace PandoranimeService
+{
+    // All the code in this file is only included on Mac Catalyst.
+    public class PlatformClass1
+    {
+    }
+}
